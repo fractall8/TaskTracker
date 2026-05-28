c4b529353d0ff27bed4f9c7d596046d0ca6a6ecd
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-05-28 16:19:30 +0300

    feat: create script for migrations

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("REQUIRED – use DbUp scripts for migrations. EF of Dapper for\ndata access");
+﻿using System.Reflection;
+using DbUp;
+
+Console.WriteLine("Starting migration...");
+
+var connectionString = args.FirstOrDefault() ?? "here connection string"; // local docker move to .env or smth 
+
+EnsureDatabase.For.PostgresqlDatabase(connectionString);
+
+var upgrader = DeployChanges.To
+    .PostgresqlDatabase(connectionString)
+    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+    .LogToConsole()
+    .Build();
+    
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.WriteLine($"An error occurred while migrating the database: {result.Error}");
+    return -1;
+}    
+
+Console.WriteLine("Migration succeeded!");
+return 0;
---
+++ Database/Scripts/0001_Initial_Create.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE "Boards"
+(
+    "Id"        UUID PRIMARY KEY,
+    "Name"      VARCHAR(100)             NOT NULL,
+    "CreatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "Columns"
+(
+    "Id"        UUID PRIMARY KEY,
+    "BoardId"   UUID                     NOT NULL,
+    "Name"      VARCHAR(50)              NOT NULL,
+    "Position"  INT                      NOT NULL,
+    "CreatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FK_Columns_Boards"
+        FOREIGN KEY ("BoardId")
+            REFERENCES "Boards" ("Id")
+            ON DELETE CASCADE
+);
+
+CREATE INDEX "IX_Columns_BoardId" ON "Columns" ("BoardId");
+
+CREATE TABLE "Tasks"
+(
+    "Id"          UUID PRIMARY KEY,
+    "ColumnId"    UUID                     NOT NULL,
+    "Title"       VARCHAR(200)             NOT NULL,
+    "Description" TEXT NULL,
+    "Position"    INT                      NOT NULL,
+    "CreatedAt"   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FK_Tasks_Columns"
+        FOREIGN KEY ("ColumnId")
+            REFERENCES "Columns" ("Id")
+            ON DELETE CASCADE
+);
+
+CREATE INDEX "IX_Tasks_ColumnId" ON "Tasks" ("ColumnId");
