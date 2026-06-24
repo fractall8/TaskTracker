b63d897d61acf7dc820d62d45217b7c33ced57be
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-06-24 12:12:11 +0300

    feat: add interfaces for api comunication on frontend

---
+++ src/Frontend/Services.Abstractions/Auth/IProfileApiService.cs
@@ -0,0 +1,11 @@
+﻿using Contracts.Requests;
+using Refit;
+
+namespace Services.Abstractions.Auth;
+
+public interface IProfileApiService
+{
+    Task UpdateProfileAsync(UpdateProfileRequest request, CancellationToken ct = default);
+    Task<string> UploadAvatarAsync(StreamPart stream, CancellationToken ct = default);
+    Task DeleteAvatarAsync(CancellationToken ct = default);
+}
