c9d658dd807268fca47b4e1fcee48ad26d68fe11
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-07-08 13:47:03 +0300

    feat: add fields for archivation to board entity and create migration

---
+++ src/Backend/Database/Scripts/0015_AddArchivationFields.sql
@@ -0,0 +1,13 @@
+﻿BEGIN;
+
+ALTER TABLE "Boards"
+    ADD COLUMN "IsArchived" BOOLEAN NOT NULL DEFAULT false,
+    ADD COLUMN "ArchivedAt" TIMESTAMP WITH TIME ZONE NULL;
+
+ALTER TABLE "Boards"
+    ADD CONSTRAINT "CK_Boards_Archive" CHECK (
+        ("IsArchived" = false AND "ArchivedAt" IS NULL) OR
+        ("IsArchived" = true AND "ArchivedAt" IS NOT NULL)
+        );
+
+COMMIT;
