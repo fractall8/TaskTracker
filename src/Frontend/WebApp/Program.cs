fb942fb139dd76a0bd75cd47fbadebe23f43e8e9
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-07-17 14:24:35 +0300

    style: create own custom toast notifications

--- src/Frontend/WebApp/Program.cs
+++ src/Frontend/WebApp/Program.cs
@@ -4,6 +4,7 @@ using MudBlazor;
 using MudBlazor.Services;
 using Services.Configuration;
 using Services.DI;
+using WebApp.Shared.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<WebApp.App>("#app");
@@ -18,7 +19,13 @@ builder.Services.AddMudServices(config =>
     config.SnackbarConfiguration.VisibleStateDuration = 4000;
     config.SnackbarConfiguration.HideTransitionDuration = 200;
     config.SnackbarConfiguration.ShowTransitionDuration = 200;
+
     config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+
+    config.SnackbarConfiguration.SuccessIcon = Icons.Material.Outlined.CheckCircleOutline;
+    config.SnackbarConfiguration.ErrorIcon = Icons.Material.Outlined.ErrorOutline;
+    config.SnackbarConfiguration.InfoIcon = Icons.Material.Outlined.Info;
+    config.SnackbarConfiguration.WarningIcon = Icons.Material.Outlined.WarningAmber;
 });
 
 builder.Services.AddWebAppServices(builder.Configuration);
@@ -35,5 +42,7 @@ builder.Services.AddMsalAuthentication(options =>
 
     options.ProviderOptions.LoginMode = "redirect";
 });
+
+builder.Services.AddScoped<CustomToastService>();
 
 await builder.Build().RunAsync();
---
+++ src/Frontend/WebApp/Shared/Toast/Models/ToastMessage.cs
@@ -0,0 +1,11 @@
+﻿using WebApp.Shared.Toast.Enums;
+
+namespace WebApp.Shared.Toast.Models;
+
+public class ToastMessage
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Message { get; set; } = string.Empty;
+    public ToastType Type { get; set; } = ToastType.Info;
+    public bool IsClosing { get; set; }
+}
